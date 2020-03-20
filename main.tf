b1d631024e52a74d7ab0e111a0fc4db1edd913b3
Kyle Johnson <kjohnson@gnulnx.net>
2020-03-20 12:43:19 -0400

    Bump up root disk to 50gb

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "aws_launch_configuration" "main" {
 
   root_block_device {
     volume_type = "gp2"
-    volume_size = 25
+    volume_size = 50
     delete_on_termination = true
   }
 }
